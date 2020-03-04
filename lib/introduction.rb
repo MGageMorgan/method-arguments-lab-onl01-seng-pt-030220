93ffabaacd9f145354d2b2a51f55f22649e808f7
Gage Morgan <gage@gages.blog>
2020-03-04 04:00:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,3 +5,6 @@ end
 def introduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
+
+introduction("Josh")
+introduction_with_language("Dan", "Ember.js")
